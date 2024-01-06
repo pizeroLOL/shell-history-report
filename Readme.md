731d9f993747fc90ca6a14fe80d206f512ec5f06
Pizero <zhaory200707@outlook.com>
2024-01-06 15:41:08 +0800

    更小的 main fn

--- Readme.md
+++ Readme.md
@@ -16,16 +16,6 @@ sudo              7         36
 cargo             8         35
 git               9         34
 up                10        27
-nvim              11        27
-cat               12        26
-python            13        25
-mkdir             14        20
-echo              15        17
-source            16        17
-poetry            17        17
-code              18        15
-chmod             19        15
-flatpak           20        14
 ..others          ..        442
 =========================================
 zsh               1         1087
--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,9 @@ fn main() {
     };
     let Outputs { shells, cmds } = merge_maps(outputs);
     start();
-    cmds.iter()
-        .take(20)
-        .enumerate()
-        .for_each(|(index, (cmd, count))| println!("{cmd:16}  {:<8}  {count}", index + 1));
-    let others = cmds
-        .iter()
-        .skip(20)
-        .map(|(_cmd, count)| count)
-        .sum::<usize>();
-    let dots = "..";
-    let txt = "..others";
-    println!("{txt:16}  {dots:<8}  {others}");
-    println!("=========================================");
-    shells
-        .iter()
-        .enumerate()
-        .for_each(|(index, (shell, count))| println!("{shell:16}  {:<8}  {count}", index + 1));
-    println!("=========================================");
-    let total = shells.iter().map(|(_, count)| count).sum::<usize>();
-    let space = "";
-    let text = "Total";
-    println!("{text:16}  {space:8}  {total}");
+    show_cmds(cmds);
+    show_shells(&shells);
+    show_totals(shells);
 }
 
 fn collect_hitorys() -> Result<Vec<ShellHistoryOutput>, Vec<String>> {
@@ -95,5 +76,37 @@ fn start() {
     let cmd = "Command";
     println!("=========================================");
     println!("{cmd:16}  {index:<8}  Count");
+    println!("=========================================");
+}
+
+fn show_cmds(cmds: Vec<(String, usize)>) {
+    cmds.iter()
+        .take(20)
+        .enumerate()
+        .for_each(|(index, (cmd, count))| println!("{cmd:16}  {:<8}  {count}", index + 1));
+    let others = cmds
+        .iter()
+        .skip(20)
+        .map(|(_cmd, count)| count)
+        .sum::<usize>();
+    let dots = "..";
+    let txt = "..others";
+    println!("{txt:16}  {dots:<8}  {others}");
+    println!("=========================================");
+}
+
+fn show_shells(shells: &Vec<(String, usize)>) {
+    shells
+        .iter()
+        .enumerate()
+        .for_each(|(index, (shell, count))| println!("{shell:16}  {:<8}  {count}", index + 1));
+    println!("=========================================");
+}
+
+fn show_totals(shells: Vec<(String, usize)>) {
+    let total = shells.iter().map(|(_, count)| count).sum::<usize>();
+    let space = "";
+    let text = "Total";
+    println!("{text:16}  {space:8}  {total}");
     println!("=========================================");
 }
